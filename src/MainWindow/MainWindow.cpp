880f0348300de9b577f00cefccbbb7a219b2ed83
nikola.roev@gmail.com <nikola.roev@gmail.com>
2022-04-27 14:17:19 +0300

    UI setup, finishing up setup.

--- src/MainWindow/MainWindow.cpp
+++ src/MainWindow/MainWindow.cpp
@@ -35,9 +35,22 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWi
 
 
 	//Set the various Combo Box Items data.
-	ui->statusListsComboBox->setItemData(0, "Reading");
-	ui->statusListsComboBox->setItemData(1, "Completed");
+	//Works, filters.
+	ui->worksFilterStatusComboBox->setItemData(0, "Any Status");
+	ui->worksFilterStatusComboBox->setItemData(1, "Reading");
+	ui->worksFilterStatusComboBox->setItemData(2, "Completed");
 
+	ui->worksFilterTypeComboBox->setItemData(0, "Any Type");
+	ui->worksFilterTypeComboBox->setItemData(1, "Series");
+	ui->worksFilterTypeComboBox->setItemData(2, "One Shot");
+	ui->worksFilterTypeComboBox->setItemData(3, "Anthology");
+
+	ui->worksFilterByComboBox->setItemData(0, "Title");
+	ui->worksFilterByComboBox->setItemData(1, "Chapter");
+	ui->worksFilterByComboBox->setItemData(2, "Creator");
+	ui->worksFilterByComboBox->setItemData(3, "Grouping");
+
+	//Works, selected work.
 	ui->statusComboBox->setItemData(0, "Reading");
 	ui->statusComboBox->setItemData(1, "Completed");
 
@@ -45,13 +58,6 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWi
 	ui->typeComboBox->setItemData(1, "One Shot");
 	ui->typeComboBox->setItemData(2, "Anthology");
 
-	ui->filterByComboBox->setItemData(0, "Title");
-	ui->filterByComboBox->setItemData(1, "Creator");
-	ui->filterByComboBox->setItemData(2, "Grouping");
-
-	ui->filterTypeComboBox->setItemData(0, "Series");
-	ui->filterTypeComboBox->setItemData(1, "One Shot");
-	ui->filterTypeComboBox->setItemData(2, "Anthology");
 
 
 	//Load settings.
@@ -145,13 +151,41 @@ void MainWindow::on_updateSearchLineEdit_textChanged(const QString& text) {
 //==================================================================================================================================
 //==================================================================================================================================
 
-void MainWindow::on_listsListWidget_itemSelectionChanged() {
+void MainWindow::on_worksFilterLineEdit_textEdited(const QString& text) {
+	qDebug() << text;
+}
+
+//==================================================================================================================================
+
+void MainWindow::on_worksFilterStatusComboBox_currentIndexChanged(int index) {
+	QString status = ui->worksFilterStatusComboBox->itemData(index).toString();
+	qDebug() << status;
+}
+
+//==================================================================================================================================
+
+void MainWindow::on_worksFilterTypeComboBox_currentIndexChanged(int index) {
+	QString type = ui->worksFilterTypeComboBox->itemData(index).toString();
+	qDebug() << type;
+}
+
+//==================================================================================================================================
+
+void MainWindow::on_worksFilterByComboBox_currentIndexChanged(int index) {
+	QString by = ui->worksFilterByComboBox->itemData(index).toString();
+	qDebug() << by;
+}
+
+//==================================================================================================================================
+//==================================================================================================================================
+
+void MainWindow::on_worksListWidget_itemSelectionChanged() {
 	qDebug("me");
 }
 
 //==================================================================================================================================
 
-void MainWindow::on_listsListWidget_itemDoubleClicked(QListWidgetItem* item) {
+void MainWindow::on_worksListWidget_itemDoubleClicked(QListWidgetItem* item) {
 	int result = QMessageBox::warning(this,
 									  "Deleting Entry.",
 									  QString("Are you sure you want to delete \"%1\"?").arg(item->text()),
@@ -165,38 +199,105 @@ void MainWindow::on_listsListWidget_itemDoubleClicked(QListWidgetItem* item) {
 
 //==================================================================================================================================
 //==================================================================================================================================
+
+void MainWindow::on_worksAddButton_clicked() {
+
+}
+
 //==================================================================================================================================
 
-void MainWindow::on_statusListsComboBox_currentIndexChanged(int index) {
-	QString status = ui->statusListsComboBox->itemData(index).toString();
-	qDebug() << status;
+void MainWindow::on_worksNameLineEdit_textEdited(const QString& text) {
+
 }
 
-void MainWindow::on_filterByComboBox_currentIndexChanged(int index) {
-	QString by = ui->filterByComboBox->itemData(index).toString();
-	qDebug() << by;
-}
-
-void MainWindow::on_filterTypeComboBox_currentIndexChanged(int index) {
-	QString type = ui->filterTypeComboBox->itemData(index).toString();
-	qDebug() << type;
-}
-
+//==================================================================================================================================
 
 void MainWindow::on_statusComboBox_currentIndexChanged(int index) {
 	QString status = ui->statusComboBox->itemData(index).toString();
 	qDebug() << status;
 }
 
+//==================================================================================================================================
 
 void MainWindow::on_typeComboBox_currentIndexChanged(int index) {
 	QString type = ui->typeComboBox->itemData(index).toString();
 	qDebug() << type;
 }
 
+//==================================================================================================================================
 
+void MainWindow::on_worksGroupingLineEdit_textEdited(const QString& text) {
 
+}
 
+//==================================================================================================================================
 
+void MainWindow::on_worksChapterLineEdit_textEdited(const QString& text) {
 
+}
 
+//==================================================================================================================================
+
+void MainWindow::on_worksAuthorEditButton_clicked() {
+
+}
+
+//==================================================================================================================================
+
+void MainWindow::on_worksArtistEditButton_clicked() {
+
+}
+
+//==================================================================================================================================
+
+void MainWindow::on_worksAuthorListWidget_itemClicked(QListWidgetItem* item) {
+
+}
+
+//==================================================================================================================================
+
+void MainWindow::on_worksArtistListWidget_itemClicked(QListWidgetItem* item) {
+
+}
+
+//==================================================================================================================================
+//==================================================================================================================================
+//==================================================================================================================================
+
+void MainWindow::on_creatorsFilterLineEdit_textEdited(const QString& text) {
+
+}
+
+//==================================================================================================================================
+
+void MainWindow::on_creatorsListWidget_itemSelectionChanged() {
+
+}
+
+//==================================================================================================================================
+
+void MainWindow::on_creatorsListWidget_itemDoubleClicked(QListWidgetItem* item) {
+
+}
+
+//==================================================================================================================================
+
+void MainWindow::on_creatorsAddButton_clicked() {
+
+}
+
+//==================================================================================================================================
+
+void MainWindow::on_creatorsNameLineEdit_textEdited(const QString& text) {
+
+}
+
+//==================================================================================================================================
+
+void MainWindow::on_creatorsWorksListWidget_itemClicked(QListWidgetItem* item) {
+
+}
+
+//==================================================================================================================================
+//==================================================================================================================================
+//==================================================================================================================================
--- src/MainWindow/MainWindow.h
+++ src/MainWindow/MainWindow.h
@@ -24,22 +24,42 @@ public:
 	~MainWindow();
 
 private slots:
+	//Menu bar.
 	void on_actionNew_triggered();
 	void on_actionOpen_triggered();
 	void on_actionClose_triggered();
 	void on_actionExit_triggered();
 
+	//Update tab.
 	void on_updateSearchLineEdit_textChanged(const QString& text);
 
-	void on_listsListWidget_itemSelectionChanged();
-	void on_listsListWidget_itemDoubleClicked(QListWidgetItem* item);
+	//Works tab.
+	void on_worksFilterLineEdit_textEdited(const QString& text);
+	void on_worksFilterStatusComboBox_currentIndexChanged(int index);
+	void on_worksFilterTypeComboBox_currentIndexChanged(int index);
+	void on_worksFilterByComboBox_currentIndexChanged(int index);
 
-	void on_statusListsComboBox_currentIndexChanged(int index);
-	void on_filterTypeComboBox_currentIndexChanged(int index);
-	void on_filterByComboBox_currentIndexChanged(int index);
+	void on_worksListWidget_itemSelectionChanged();
+	void on_worksListWidget_itemDoubleClicked(QListWidgetItem* item);
 
+	void on_worksAddButton_clicked();
+	void on_worksNameLineEdit_textEdited(const QString& text);
 	void on_statusComboBox_currentIndexChanged(int index);
 	void on_typeComboBox_currentIndexChanged(int index);
+	void on_worksGroupingLineEdit_textEdited(const QString& text);
+	void on_worksChapterLineEdit_textEdited(const QString& text);
+	void on_worksAuthorEditButton_clicked();
+	void on_worksArtistEditButton_clicked();
+	void on_worksAuthorListWidget_itemClicked(QListWidgetItem* item);
+	void on_worksArtistListWidget_itemClicked(QListWidgetItem* item);
+
+	//Creator tab.
+	void on_creatorsFilterLineEdit_textEdited(const QString& text);
+	void on_creatorsListWidget_itemSelectionChanged();
+	void on_creatorsListWidget_itemDoubleClicked(QListWidgetItem* item);
+	void on_creatorsAddButton_clicked();
+	void on_creatorsNameLineEdit_textEdited(const QString& text);
+	void on_creatorsWorksListWidget_itemClicked(QListWidgetItem* item);
 };
 
 //==================================================================================================================================
