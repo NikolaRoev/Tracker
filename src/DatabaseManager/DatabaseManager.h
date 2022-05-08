a7b02275392bcce41b321ebfe03054b287bd90c5
nikola.roev@gmail.com <nikola.roev@gmail.com>
2022-05-08 16:20:07 +0300

    Some fix ups. Stylesheet setup.

--- src/DatabaseManager/DatabaseManager.h
+++ src/DatabaseManager/DatabaseManager.h
@@ -7,29 +7,28 @@
 
 //==================================================================================================================================
 
-class DatabaseManager {
-public:
-	static void init();
-	static void deinit();
-	static void open(const QString& name);
-	static void close();
-	static const QString get_name();
+namespace DatabaseManager {
+	void init();
+	void deinit();
+	void open(const QString& name);
+	void close();
+	const QString get_name();
 
 	//Work.
-	static void add_work(const Work& work);
-	static void remove_work(const int id);
-	static void update_work(const QString& column, const int id, const QString& value);
-	static Work get_work(const int id);
-	static QVector<Work> search_works(const QString& search, const QString& by, const QString& status, const QString& type);
+	void add_work(const Work& work);
+	void remove_work(const int id);
+	void update_work(const QString& column, const int id, const QString& value);
+	Work get_work(const int id);
+	QVector<Work> search_works(const QString& search, const QString& by, const QString& status, const QString& type);
 
 	//Creator.
-	static void add_creator(const QString& name);
-	static void remove_creator(const int id);
-	static void update_creator(const QString& column, const int id, const QString& value);
-	static Creator get_creator(const int id);
-	static QVector<Creator> search_creators(const QString& search);
-	static void attach_creator(const int work_id, const int creator_id, const QString& type);
-	static void detach_creator(const int work_id, const int creator_id);
-};
+	void add_creator(const QString& name);
+	void remove_creator(const int id);
+	void update_creator(const QString& column, const int id, const QString& value);
+	Creator get_creator(const int id);
+	QVector<Creator> search_creators(const QString& search);
+	void attach_creator(const int work_id, const int creator_id, const QString& type);
+	void detach_creator(const int work_id, const int creator_id);
+}
 
 //==================================================================================================================================
