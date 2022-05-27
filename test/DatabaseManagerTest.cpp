e6643ec28986217bd5cb7ebcf6425a96932c8d11
nikola.roev@gmail.com <nikola.roev@gmail.com>
2022-05-27 13:37:22 +0300

    Scuffed QTest setup.

---
+++ test/DatabaseManagerTest.cpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <QTest>
+
+class DatabaseManagerTest: public QObject {
+	Q_OBJECT
+private slots:
+	void initTestCase()
+	{
+		qDebug("Called before everything else.");
+	}
+
+	void firstTest()
+	{
+		QVERIFY(true); // check that a condition is satisfied
+		QCOMPARE(1, 1); // compare two values
+	}
+
+	void cleanupTestCase()
+	{
+		qDebug("Called after myFirstTest and mySecondTest.");
+	}
+};
+
+QTEST_APPLESS_MAIN(DatabaseManagerTest)
+#include "./DatabaseManagerTest.moc"
