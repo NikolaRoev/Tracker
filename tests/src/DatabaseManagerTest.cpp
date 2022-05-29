1a0c766c82f1b217cc9508f7f661f19373f1c6e2
nikola.roev@gmail.com <nikola.roev@gmail.com>
2022-05-30 00:24:56 +0300

    Some tests setup.

--- tests/src/DatabaseManagerTest.cpp
+++ tests/src/DatabaseManagerTest.cpp
@@ -5,26 +5,28 @@
 #include "DatabaseManager/Work.h"
 #include "DatabaseManager/Creator.h"
 
+//==================================================================================================================================
+
 class DatabaseManagerTest: public QObject {
 	Q_OBJECT
 private slots:
-	void initTestCase()
-	{
-		DatabaseManager::init("test.db");
-		qDebug("Called before everything.");
+	void initTestCase() {
+		QVERIFY(DatabaseManager::init("test.db"));
 	}
 
-	void firstTest()
-	{
-		QVERIFY(true); // check that a condition is satisfied
-		QCOMPARE(1, 1); // compare two values
+	void firstTest() {
+
 	}
 
-	void cleanupTestCase()
-	{
-		qDebug("Called after myFirstTest and mySecondTest.");
+	void cleanupTestCase() {
+		DatabaseManager::deinit();
+		QVERIFY(QFile::remove("test.db"));
 	}
 };
 
+//==================================================================================================================================
+
 QTEST_APPLESS_MAIN(DatabaseManagerTest)
 #include "./DatabaseManagerTest.moc"
+
+//==================================================================================================================================
