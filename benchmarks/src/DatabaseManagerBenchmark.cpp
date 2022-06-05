9db647ec15355eaaf345008c17d2038f6dc26765
nikola.roev@gmail.com <nikola.roev@gmail.com>
2022-06-05 16:25:15 +0300

    Added benchmarking.

---
+++ benchmarks/src/DatabaseManagerBenchmark.cpp
@@ -0,0 +1,60 @@
+#pragma once
+#include <QTest>
+
+#include "DatabaseManager/DatabaseManager.h"
+#include "DatabaseManager/Work.h"
+#include "DatabaseManager/Creator.h"
+
+//==================================================================================================================================
+
+class DatabaseManagerBenchmark: public QObject {
+	Q_OBJECT
+private slots:
+	void initTestCase() {
+		QVERIFY(DatabaseManager::init("test.db"));
+	}
+
+	void cleanup() {
+		QSqlQuery query;
+		QVERIFY(query.exec("DELETE FROM works"));
+		QVERIFY(query.exec("DELETE FROM creators"));
+		QVERIFY(query.exec("DELETE FROM work_creator"));
+	}
+
+
+	//Work.
+	void add_workTest() {
+
+	}
+
+	void remove_workTest() {
+
+	}
+
+	void update_workTest() {
+
+	}
+
+	void get_workTest() {
+
+	}
+
+	void search_worksTest() {
+
+	}
+
+
+	//Creator.
+
+	void cleanupTestCase() {
+		DatabaseManager::deinit();
+		QVERIFY(QFile::remove("test.db"));
+	}
+};
+
+//==================================================================================================================================
+
+QTEST_APPLESS_MAIN(DatabaseManagerBenchmark)
+#include "./DatabaseManagerBenchmark.moc"
+
+//==================================================================================================================================
