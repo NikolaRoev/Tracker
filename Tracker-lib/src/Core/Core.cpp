ee7437aa2d8eef34c8e19704fe05a21147e7c5f0
nikola.roev@gmail.com <nikola.roev@gmail.com>
2022-06-18 10:23:14 +0300

    Moved the main starting code into a Core wrapper.

---
+++ Tracker-lib/src/Core/Core.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "Core.h"
+#include "MainWindow/MainWindow.h"
+
+//==================================================================================================================================
+
+int Core::start(int& argc, char** argv) {
+	qSetMessagePattern("[%{if-debug}Debug%{endif}"
+					   "%{if-info}Info%{endif}"
+					   "%{if-warning}Warning%{endif}"
+					   "%{if-critical}Critical%{endif}"
+					   "%{if-fatal}Fatal%{endif}]"
+					   "[%{time hh:mm:ss.zzz}][%{file}:%{line}][%{function}]:\n%{message}");
+
+	QApplication app(argc, argv);
+
+	QFile file(":/stylesheets/stylesheets/stylesheet.qss");
+	if (!file.open(QFile::ReadOnly)) {
+		qWarning() << file.errorString();
+	}
+	//app.setStyleSheet(file.readAll());
+
+	MainWindow window;
+	window.show();
+	return app.exec();
+}
+
+//==================================================================================================================================
