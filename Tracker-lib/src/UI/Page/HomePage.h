67080070d52380f4704c9d193853fba7be299a24
nikola.roev@gmail.com <nikola.roev@gmail.com>
2022-07-02 21:01:49 +0300

    ...

--- Tracker-lib/src/UI/Page/HomePage.h
+++ Tracker-lib/src/UI/Page/HomePage.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #pragma once
 #include "pch.h"
 #include "Page.h"
@@ -32,38 +31,3 @@ private slots:
 	void on_tableWidget_clicked(const QModelIndex& index);
 	void on_tableWidget_customContextMenuRequested(const QPoint& pos);
 };
-=======
-#pragma once
-#include "pch.h"
-#include "Page.h"
-#include "DatabaseManager/Work.h"
-#include "DatabaseManager/Creator.h"
-
-QT_BEGIN_NAMESPACE
-namespace Ui {
-	class HomePage;
-}
-QT_END_NAMESPACE
-
-class HomePage : public Page {
-	Q_OBJECT
-private:
-	Ui::HomePage* ui{ nullptr };
-	int id{};
-
-public:
-	HomePage(QWidget* parent = nullptr);
-	~HomePage();
-
-	void populate() override;
-
-private slots:
-	void on_lineEdit_textEdited(const QString&);
-	void on_whatComboBox_currentIndexChanged(int index);
-	void on_statusComboBox_currentIndexChanged(int);
-	void on_typeComboBox_currentIndexChanged(int);
-	void on_byComboBox_currentIndexChanged(int);
-	void on_tableWidget_clicked(const QModelIndex& index);
-	void on_tableWidget_customContextMenuRequested(const QPoint& pos);
-};
->>>>>>> 729b44e6c4fd5284eea94280af1970e131fcd6ad
